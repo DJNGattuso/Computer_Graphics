7f952ae82431668c684f6962b55a1bf9f8af2cb0
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-10-13 03:24:54 -0400

    Adjusted the shaders to receive a colour vector that represents a points colourPoints
    Create a function to make the points and to get the respective colours
    
    fixed a bit the camera position
    
    added the skip step size

--- Assignment2/comp371/Main.cpp
+++ Assignment2/comp371/Main.cpp
@@ -26,7 +26,7 @@ glm::mat4 projection_matrix;
 bool optionRight = false, optionLeft = false, optionMid = false; //variables to track the mouse click
 
 //Variables for camera ------------------------------------------------------------------------------------------------------
-const glm::vec3 center(0.0f, 50.0f, -60.0f);
+const glm::vec3 center(50.0f, 250.0f, 100.0f);
 const glm::vec3 up(0.0f, 1.0f, 0.0f);
 float panX = 0.0f, tiltY = 0.0f, zoom = 1.0f;		//variables that will be adjusted through cursor callback
 float rotAnglex = 0.0f, rotAngley = 0.0f;			//variables to be adjusted through key callback
@@ -38,7 +38,11 @@ void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
 double oldPositiony = 0; double oldPositionx = 0;
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mode);
 
-void restartGame(); //function to restart the game
+//functions to generate points in map-------------------------------
+vector<glm::vec3> createPoints(CImg<float> image, int skip);
+vector<glm::vec3> colourPoints(vector<glm::vec3> points);
+
+int tag = 1; //variable that will allow to change what step is being drawn
 
 
 // The MAIN function---------------------------------------------------------------------------------------------------------------
@@ -170,20 +174,13 @@ int main()
 	CImgDisplay main_disp(image, "The image");
 
 	//---------------------Get Pixel Data-----------------------------------
-	vector <glm::vec3> imagePoints;
+	vector <glm::vec3> imagePoints = createPoints(image, 1);
+	vector <glm::vec3> pointsColour = colourPoints(imagePoints);
 
-	int x = (0 - image.width()/2); int z = (0 - image.height() / 2);
-	for (CImg<float>::iterator i = image.begin(); i < image.end(); i++)
-	{
-		imagePoints.emplace_back(glm::vec3(x++, *i, z));
-		if (x == image.width()) { x = (0 - image.width() / 2); z += 1; }
-	}
-
-	cout << imagePoints.size() << endl;
-
-	GLuint VAO_Image, VBO_Image;
+	GLuint VAO_Image, VBO_Image, VBO_Colour;
 	glGenVertexArrays(1, &VAO_Image);
 	glGenBuffers(1, &VBO_Image);
+	glGenBuffers(1, &VBO_Colour);
 
 	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
 	glBindVertexArray(VAO_Image);
@@ -192,8 +189,45 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, imagePoints.size() * sizeof(glm::vec3), &imagePoints.front(), GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_Colour);
+	glBufferData(GL_ARRAY_BUFFER, pointsColour.size() * sizeof(glm::vec3), &pointsColour.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 1);
+
+	glBindVertexArray(0); // Unbind VAO
+
+	//-------------------------------------------Setup for points with skip size-----------------------------------------------------
+	int skipsize;
+	cout << "\nPlease enter a skip-size: ";
+	cin >> skipsize;
+
+	vector<glm::vec3> skipPoints = createPoints(image, skipsize);
+	vector<glm::vec3> skipColour = colourPoints(skipPoints);
+
+	GLuint VAO_Skip, VBO_Skip, VBO_SkipColour;
+	glGenVertexArrays(1, &VAO_Skip);
+	glGenBuffers(1, &VBO_Skip);
+	glGenBuffers(1, &VBO_SkipColour);
+
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO_Skip);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_Skip);
+	glBufferData(GL_ARRAY_BUFFER, skipPoints.size() * sizeof(glm::vec3), &skipPoints.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_SkipColour);
+	glBufferData(GL_ARRAY_BUFFER, skipColour.size() * sizeof(glm::vec3), &skipColour.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 1);
 
 	glBindVertexArray(0); // Unbind VAO
 
@@ -212,37 +246,6 @@ int main()
 	stbi_image_free(data);
 	*/
 
-	float vertices[] = {
-		0.5f, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		-0.5f, 0.5f, 0.0f
-	};
-	unsigned int indices[] = {
-		//first triangle
-		0, 1, 3,
-		//second triangle
-		1, 2, 3
-	};
-
-	unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-	glBindVertexArray(VAO); //bind the Vertex Array object
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO); //bind the vertex buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //copy defined vertex data into buffer memory
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	//linking vertex attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); //3 vertex of 3 positions each of 4 bytes
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
 
 	//get the uniform locations from the shader ----------------------------------------------------------------------------------
 	GLuint projectionLoc = glGetUniformLocation(shaderProgram, "projection_matrix");
@@ -253,9 +256,11 @@ int main()
 	//enable for depth testing
 	//glEnable(GL_BLEND | GL_DEPTH_TEST);
 
+
 	// Game loop------------------------------------------------------------------------------------------------------------------
 	while (!glfwWindowShouldClose(window))
 	{
+
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
 
@@ -267,7 +272,7 @@ int main()
 		//set the camera-----------------------------------------------------------------------------------------
 		glm::mat4 view_matrix;
 		glm::mat4 model_matrix;
-		glm::vec3 eye(20.0f, 70.0f + panX, -100.0f - zoom);
+		glm::vec3 eye(0.0f + panX, 100.0f + tiltY, 0.0f + zoom);
 		view_matrix = glm::lookAt(eye, center, up);
 
 		/*
@@ -283,19 +288,21 @@ int main()
 		glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, glm::value_ptr(view_matrix));
 		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection_matrix));
 
-		glUniform1i(object_type_loc, 2);
-		glBindVertexArray(VAO_Image);
-		glDrawArrays(GL_POINTS, 0, imagePoints.size());
-		glBindVertexArray(0);
-
-		
-		glUniform1i(object_type_loc, 2);
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glBindVertexArray(0);
-		
-		
-
+		switch (tag)
+		{
+			case 1: 
+			glBindVertexArray(VAO_Image);
+			glDrawArrays(GL_POINTS, 0, imagePoints.size());
+			glBindVertexArray(0);
+			break;
+			case 2:
+				glBindVertexArray(VAO_Skip);
+				glDrawArrays(GL_POINTS, 0, imagePoints.size());
+				glBindVertexArray(0);
+				break;
+		}
+	
+	
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
@@ -306,12 +313,82 @@ int main()
 	
 }
 
-	//key callback function------------------------------------------------------------------------------------------------------------
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+//------------------------------------------Function to generate image points based on a skip----------------------------------------
+vector<glm::vec3> createPoints(CImg<float> image, int skip)
+{
+	vector<glm::vec3> newPoints;
+
+	int x = (0 - image.width() / 2); int z = (0 - image.height() / 2);
+
+	//------------------------Loop through each pixel in the image----------------------------
+	for (CImg<float>::iterator i = image.begin(); i < image.end(); i++)
+	{
+		//--------------Setup to get the heightmap representation of the pixel----------------
+		newPoints.emplace_back(glm::vec3(x++, *i, z));
+		if (skip > (abs(x - image.width())))
+		{
+			x = (0 - image.width() / 2); z += 1;
+		}
+		else if (x == image.width()) { x = (0 - image.width() / 2); z += 1; }
+	}
+
+	return newPoints;
+}
+
+//------------------------------------------Function to generate image points based on a skip----------------------------------------
+vector<glm::vec3> colourPoints(vector<glm::vec3> points)
+{
+	vector<glm::vec3> pointsColour;
+
+	for (int i = 0; i <= points.size() - 1; i++)
+	{
+		//------------------------Setup to get colour for each point---------------------------
+		if (points[i].y < 67) { pointsColour.emplace_back(glm::vec3(0.0, 0.0, 1.0)); }
+		else if (67 <= points[i].y && points[i].y < 85) { pointsColour.emplace_back(glm::vec3(0.2, 0.0, 0.8)); }
+		else if (85 <= points[i].y && points[i].y < 100) { pointsColour.emplace_back(glm::vec3(0.4, 0.0, 0.6)); }
+		else if (100 <= points[i].y && points[i].y < 120) { pointsColour.emplace_back(glm::vec3(0.6, 0.0, 0.4)); }
+		else if (120 <= points[i].y && points[i].y < 140) { pointsColour.emplace_back(glm::vec3(0.8, 0.2, 0.2)); }
+		else if (140 <= points[i].y && points[i].y < 160) { pointsColour.emplace_back(glm::vec3(0.8, 0.4, 0.1)); }
+		else if (160 <= points[i].y && points[i].y < 180) { pointsColour.emplace_back(glm::vec3(1.0, 0.5, 0.1)); }
+		else if (180 <= points[i].y && points[i].y < 200) { pointsColour.emplace_back(glm::vec3(1.0, 0.7, 0.0)); }
+		else if (200 <= points[i].y && points[i].y < 220) { pointsColour.emplace_back(glm::vec3(0.7, 0.8, 0.0)); }
+		else if (220 <= points[i].y && points[i].y < 240) { pointsColour.emplace_back(glm::vec3(0.5, 0.9, 0.0)); }
+		else { pointsColour.emplace_back(glm::vec3(0.0, 1.0, 0.0)); }
+	}
+	return pointsColour;
+}
+//key callback function------------------------------------------------------------------------------------------------------------
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) 
+	{
 	std::cout << key << std::endl;
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, true);
+	}
+	//Change Camera--------------------------------------------------------------------------------
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		zoom -= 10.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		zoom += 10.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		panX += 10.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		panX -= 10.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
+	{
+		tiltY += 10.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+	{
+		tiltY -= 10.0f;
 	}
 	//change orientation--------------------------------------------------------------------------
 	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
@@ -330,24 +407,20 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	{
 		rotAnglex -= 0.5f;
 	}
-	//restart game --------------------------------------------------------------------------------------
-	if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
+	//Show Specific Step based on the tag------------------------------------------------------------------------------------
+	if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS) //show all points
 	{
-		restartGame();
+		tag = 1;
+	}
+	if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS) //show the skip points
+	{
+		tag = 2;
 	}
 	//fix orientation-----------------------------------------------------------------------------------
 	if (glfwGetKey(window, GLFW_KEY_HOME) == GLFW_PRESS)
 	{
 		rotAnglex = 0.0f; rotAngley = 0.0f;
 		panX = 0.0f; tiltY = 0.0f; zoom = 1.0f;
-	}
-	if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
-	{
-		zoom -= 5.0f;
-	}
-	if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
-	{
-		panX += 5.0f;
 	}
 }
 
