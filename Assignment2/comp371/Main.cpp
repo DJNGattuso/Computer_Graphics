bfb127424d015f9c4177f03443c5f216b86621ac
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-10-17 13:33:35 -0400

    Major changes. Couldn't commit in pieces when I was writing it
    fixed titles for shader
    
    added catmull rom along z
    
    added height and width tracking for when the skip happens
    
    allowed catmull to be applied to both original and skipped
    
    output any text using the keys and get changes though new inputs

--- Assignment2/comp371/Main.cpp
+++ Assignment2/comp371/Main.cpp
@@ -39,14 +39,16 @@ double oldPositiony = 0; double oldPositionx = 0;
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mode);
 
 //functions to generate points in map-------------------------------
-vector<glm::vec3> createPoints(vector<glm::vec3> original, int skip);
+vector<glm::vec3> createPoints(vector<glm::vec3> original, int skip, int width, int height);
 vector<glm::vec3> colourPoints(vector<glm::vec3> points);
 vector<glm::vec3> CatmullRomX(vector<glm::vec3> points, float step, int height, int width);
+vector<glm::vec3> CatmullRom(vector<glm::vec3> points, float step, int height, int width);
 vector<GLuint> getTri(vector<glm::vec3> points, int height, int width);
 
 //variables to control the environment-------------------------------------------------------------------------------------------
 int tag = 1; //variable that will allow to change what step is being drawn
 bool tri = false;
+bool ori = false;
 bool getInputs = false;
 int skipsize;
 float stepsize;
@@ -101,7 +103,7 @@ int main()
 	// Build and compile our shader program-----------------------------------------------------------------------------
 	// Vertex shader
 	// Read the Vertex Shader code from the file
-	string vertex_shader_path = "vertexShaderPacman.vs.txt";
+	string vertex_shader_path = "vertexShader.vs.txt";
 	string VertexShaderCode;
 	std::ifstream VertexShaderStream(vertex_shader_path, ios::in);
 
@@ -118,7 +120,7 @@ int main()
 	}
 
 	// Read the Fragment Shader code from the file
-	string fragment_shader_path = "fragmentShaderPacman.fs.txt";
+	string fragment_shader_path = "fragmentShader.fs.txt";
 	std::string FragmentShaderCode;
 	std::ifstream FragmentShaderStream(fragment_shader_path, std::ios::in);
 
@@ -186,9 +188,9 @@ int main()
 
 	//--------------------Loop through each pixel in the image----------------
 	cout << "Generating pixels" << endl;
-	for (int x = (0 - image.width() / 2); x < (image.width())/2; x++)
+	for (int z = (0 - image.height() / 2); z < (image.height()) / 2; z++)
 	{
-		for (int z = (0 - image.height() / 2); z < (image.height()) / 2; z++)
+		for (int x = (0 - image.width() / 2); x < (image.width()) / 2; x++)
 		{
 			float height = static_cast<float>(*image.data(x + (image.width() / 2), z + (image.height() / 2)));
 			imagePoints.emplace_back(glm::vec3(x, height, z));
@@ -232,8 +234,11 @@ int main()
 	cout << "\nPlease enter a skip-size: ";
 	cin >> skipsize;
 
-	vector<glm::vec3> skipPoints = createPoints(imagePoints, skipsize);
+	vector<glm::vec3> skipPoints = createPoints(imagePoints, skipsize, image.width(), image.height());
 	vector<glm::vec3> skipColour = colourPoints(skipPoints);
+	
+	int skipWidth = trackWidth;
+	int skipHeight = image.height();
 
 	GLuint VAO_Skip, VBO_Skip, VBO_SkipColour;
 	glGenVertexArrays(1, &VAO_Skip);
@@ -262,7 +267,7 @@ int main()
 	cout << "\nPlease enter a step-size: ";
 	cin >> stepsize;
 
-	vector<glm::vec3> catXPoints = CatmullRomX(imagePoints, stepsize, image.height(), image.width());
+	vector<glm::vec3> catXPoints = CatmullRomX(skipPoints, stepsize, skipHeight, skipWidth);
 	vector<glm::vec3> CatXColour = colourPoints(catXPoints);
 
 	GLuint VAO_CatX, VBO_CatX, VBO_CatXColour;
@@ -287,6 +292,84 @@ int main()
 
 	glBindVertexArray(0);
 	
+	/*------------------------------------------Setup Catmull Rom for the z----------------------------------------------------------
+	vector<glm::vec3> catPoints = CatmullRomX(skipPoints, stepsize, skipHeight, skipWidth);
+	vector<glm::vec3> CatColour = colourPoints(catPoints);
+
+	GLuint VAO_Cat, VBO_Cat, VBO_CatColour;
+	glGenVertexArrays(1, &VAO_Cat);
+	glGenBuffers(1, &VBO_Cat);
+	glGenBuffers(1, &VBO_CatColour);
+
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO_Cat);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_Cat);
+	glBufferData(GL_ARRAY_BUFFER, catPoints.size() * sizeof(glm::vec3), &catPoints.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_CatColour);
+	glBufferData(GL_ARRAY_BUFFER, CatColour.size() * sizeof(glm::vec3), &CatColour.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 1);
+
+	glBindVertexArray(0);
+	*/
+	//------------------------------------------Setup Catmull Rom for the original----------------------------------------------------------
+	vector<glm::vec3> catXImage = CatmullRomX(imagePoints, stepsize, image.height(), image.width());
+	vector<glm::vec3> CatXColourI = colourPoints(catXImage);
+
+	GLuint VAO_CatXI, VBO_CatXI, VBO_CatXColourI;
+	glGenVertexArrays(1, &VAO_CatXI);
+	glGenBuffers(1, &VBO_CatXI);
+	glGenBuffers(1, &VBO_CatXColourI);
+
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO_CatXI);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_CatXI);
+	glBufferData(GL_ARRAY_BUFFER, catXImage.size() * sizeof(glm::vec3), &catXImage.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_CatXColourI);
+	glBufferData(GL_ARRAY_BUFFER, CatXColourI.size() * sizeof(glm::vec3), &CatXColourI.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 1);
+
+	glBindVertexArray(0);
+
+	//------------------------------------------Setup Catmull Rom for the z----------------------------------------------------------
+	vector<glm::vec3> catImage = CatmullRom(imagePoints, stepsize, image.height(), image.width());
+	vector<glm::vec3> CatColourI = colourPoints(catXImage);
+
+	GLuint VAO_CatI, VBO_CatI, VBO_CatColourI;
+	glGenVertexArrays(1, &VAO_CatI);
+	glGenBuffers(1, &VBO_CatI);
+	glGenBuffers(1, &VBO_CatColourI);
+
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO_CatI);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_CatI);
+	glBufferData(GL_ARRAY_BUFFER, catImage.size() * sizeof(glm::vec3), &catImage.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_CatColourI);
+	glBufferData(GL_ARRAY_BUFFER, CatColourI.size() * sizeof(glm::vec3), &CatColourI.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 1);
+
+	glBindVertexArray(0);
+
 	/*
 	int width1 = 0;
 	int height1 = 0;
@@ -323,8 +406,11 @@ int main()
 			cout << "\nPlease enter a skip-size: ";
 			cin >> skipsize;
 
-			skipPoints = createPoints(imagePoints, skipsize);
+			skipPoints = createPoints(imagePoints, skipsize, image.width(), image.height());
 			skipColour = colourPoints(skipPoints);
+
+			int skipWidth = trackWidth;
+			int skipHeight = trackHeight;
 
 			// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
 			glBindVertexArray(VAO_Skip);
@@ -361,6 +447,56 @@ int main()
 
 			glBindBuffer(GL_ARRAY_BUFFER, VBO_CatXColour);
 			glBufferData(GL_ARRAY_BUFFER, CatXColour.size() * sizeof(glm::vec3), &CatXColour.front(), GL_STATIC_DRAW);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+			glEnableVertexAttribArray(1);
+			glBindBuffer(GL_ARRAY_BUFFER, 1);
+
+			glBindVertexArray(0);
+
+			catXImage = CatmullRomX(imagePoints, stepsize, image.height(), image.width());
+			CatXColourI = colourPoints(catXImage);
+
+			GLuint VAO_CatXI, VBO_CatXI, VBO_CatXColourI;
+			glGenVertexArrays(1, &VAO_CatXI);
+			glGenBuffers(1, &VBO_CatXI);
+			glGenBuffers(1, &VBO_CatXColourI);
+
+			// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+			glBindVertexArray(VAO_CatXI);
+
+			glBindBuffer(GL_ARRAY_BUFFER, VBO_CatXI);
+			glBufferData(GL_ARRAY_BUFFER, catXImage.size() * sizeof(glm::vec3), &catXImage.front(), GL_STATIC_DRAW);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+			glBindBuffer(GL_ARRAY_BUFFER, VBO_CatXColourI);
+			glBufferData(GL_ARRAY_BUFFER, CatXColourI.size() * sizeof(glm::vec3), &CatXColourI.front(), GL_STATIC_DRAW);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+			glEnableVertexAttribArray(1);
+			glBindBuffer(GL_ARRAY_BUFFER, 1);
+
+			glBindVertexArray(0);
+
+			catImage = CatmullRom(imagePoints, stepsize, image.height(), image.width());
+			CatColourI = colourPoints(catXImage);
+
+			GLuint VAO_CatI, VBO_CatI, VBO_CatColourI;
+			glGenVertexArrays(1, &VAO_CatI);
+			glGenBuffers(1, &VBO_CatI);
+			glGenBuffers(1, &VBO_CatColourI);
+
+			// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+			glBindVertexArray(VAO_CatI);
+
+			glBindBuffer(GL_ARRAY_BUFFER, VBO_CatI);
+			glBufferData(GL_ARRAY_BUFFER, catImage.size() * sizeof(glm::vec3), &catImage.front(), GL_STATIC_DRAW);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+			glBindBuffer(GL_ARRAY_BUFFER, VBO_CatColourI);
+			glBufferData(GL_ARRAY_BUFFER, CatColourI.size() * sizeof(glm::vec3), &CatColourI.front(), GL_STATIC_DRAW);
 			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 			glEnableVertexAttribArray(1);
 			glBindBuffer(GL_ARRAY_BUFFER, 1);
@@ -444,9 +580,32 @@ int main()
 			glBindVertexArray(0);
 			break;
 		case 3:
-			glBindVertexArray(VAO_CatX);
-			glDrawArrays(GL_POINTS, 0, catXPoints.size());
-			glBindVertexArray(0);
+			if (ori)
+			{
+				glBindVertexArray(VAO_CatXI);
+				glDrawArrays(GL_POINTS, 0, catXImage.size());
+				glBindVertexArray(0);
+			}
+			else {
+				glBindVertexArray(VAO_CatX);
+				glDrawArrays(GL_POINTS, 0, catXPoints.size());
+				glBindVertexArray(0);
+			}
+			
+			break;
+		case 4:
+			if (ori)
+			{
+				glBindVertexArray(VAO_CatI);
+				glDrawArrays(GL_POINTS, 0, catImage.size());
+				glBindVertexArray(0);
+			}
+			else
+			{
+				glBindVertexArray(VAO_CatX);
+				glDrawArrays(GL_POINTS, 0, catXPoints.size());
+				glBindVertexArray(0);
+			}
 			break;
 		}
 		
@@ -466,16 +625,18 @@ int main()
 }
 
 //------------------------------------------Function to generate image points based on a skip----------------------------------------
-vector<glm::vec3> createPoints(vector<glm::vec3> original, int skip)
+vector<glm::vec3> createPoints(vector<glm::vec3> original, int skip, int width, int height)
 {
 	cout << "Generating new points with skip: " << skip << endl;
 	vector<glm::vec3> newPoints;
 
+	trackWidth = (width / skip);
 	//------------------------Loop through each pixel in the image----------------------------
 	for (int i = 0; i < original.size(); i += skip)
 	{
 		newPoints.emplace_back(original[i]);
 	}
+	trackHeight = (newPoints.size() / trackWidth);
 
 	return newPoints;
 }
@@ -505,24 +666,24 @@ vector<glm::vec3> colourPoints(vector<glm::vec3> points)
 	return pointsColour;
 }
 
-//------------------------------------------Function to generate Catmull Rom points based on a step----------------------------------------
+//------------------------------------------Function to generate Catmull Rom points along x axis----------------------------------------
 //referenced for equation from: http://hawkesy.blogspot.ca/2010/05/catmull-rom-spline-curve-implementation.html
 vector<glm::vec3> CatmullRomX(vector<glm::vec3> points, float step, int height, int width)
 {
-	//cout << "Getting Cat X with vector of size: " << points.size() << " and step of " << step << endl;
+	cout << "Getting Cat X with vector of size: " << points.size() << " and step of " << step << endl;
 	glm::vec3 point0, point1, point2, point3;
 	vector<glm::vec3> catmullRom;
 	int index = 0;
 
 	for (int j = 0; j <= height - 1; j++) //start at first row, go to next row until last
 	{
-		index = j;
-		for (int i = 0; i < ((height * width) - (5 * height)); i += height) //start at first column, go to next column until 4th to last
+		//index = j;
+		for (int index = 0; index < ((height * width) - (5 * height)); index += height) //start at first column, go to next column until 4th to last
 		{
-			point0 = points[index];
-			point1 = points[index + height];
-			point2 = points[index + height + height];
-			point3 = points[index + height + height + height];
+			point0 = points[index + j];
+			point1 = points[index + height + j];
+			point2 = points[index + height + height + j];
+			point3 = points[index + height + height + height + j];
 
 			glm::vec3 newpoint = 0.5f * ((2.0f * point1) + (point2 - point0) * step + (2.0f * point0 - 5.0f * point1 + 4.0f * point2 - point3) *
 				step * step + (3.0f * point1 - point0 - 3.0f * point2 + point3) * step * step * step);
@@ -531,7 +692,38 @@ vector<glm::vec3> CatmullRomX(vector<glm::vec3> points, float step, int height, 
 			index += height;
 		}
 	}
-	//cout << "succeeded" << endl;
+	cout << "succeeded" << endl;
+	return catmullRom;
+}
+
+//------------------------------------------Function to generate Catmull Rom points based on a step----------------------------------------
+//referenced for equation from: http://hawkesy.blogspot.ca/2010/05/catmull-rom-spline-curve-implementation.html
+vector<glm::vec3> CatmullRom(vector<glm::vec3> points, float step, int height, int width)
+{
+	cout << "Getting Cat with vector of size: " << points.size() << " and step of " << step << endl;
+	glm::vec3 point0, point1, point2, point3;
+	vector<glm::vec3> catmullRom;
+	int index = 0;
+	cout << width;
+
+	for (int i = 0; i < (height * (width/2)); i += height) //start at first column, go to next column until 4th to last
+	{
+		index = i;
+		for (int j = 0; j <= height/2 - 1; j++) //start at first row, go to next row until last
+		{
+			point0 = points[index + j];
+			point1 = points[index + j + 1];
+			point2 = points[index + j + 2];
+			point3 = points[index + j + 3];
+
+			glm::vec3 newpoint = 0.5f * ((2.0f * point1) + (point2 - point0) * step + (2.0f * point0 - 5.0f * point1 + 4.0f * point2 - point3) *
+				step * step + (3.0f * point1 - point0 - 3.0f * point2 + point3) * step * step * step);
+
+			catmullRom.emplace_back(newpoint);
+			index += height;
+		}
+	}
+	cout << "succeeded" << endl;
 	return catmullRom;
 }
 
@@ -625,6 +817,10 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	{
 		tag = 3;
 	}
+	if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS) //show the catmull rom X points
+	{
+		tag = 4;
+	}
 	if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) //get new inputs
 	{
 		getInputs = true;
@@ -634,12 +830,17 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		if (tri) { tri = false; } 
 		else { tri = true; }
 	}
-	//fix orientation-----------------------------------------------------------------------------------
-	if (glfwGetKey(window, GLFW_KEY_HOME) == GLFW_PRESS)
+	if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) //toggle between drawing original or skipped
 	{
-		rotAnglex = 0.0f; rotAngley = 0.0f;
+		if (ori) { ori = false; }
+		else { ori = true; }
+	}
+	//reset------------------------------------------------------------------------------------------
+	if (glfwGetKey(window, GLFW_KEY_BACKSPACE) == GLFW_PRESS)
+	{
 		panX = 0.0f; tiltY = 0.0f; zoom = 1.0f;
-		stepsize = 0.5;
+		tag = 1;
+		getInputs = true;
 	}
 }
 
--- Assignment2/comp371/ReadMe.txt
+++ Assignment2/comp371/ReadMe.txt
@@ -1,3 +1,37 @@
 Assignment by Nicholas Gattuso 40007087
 
-Assignment 2 represents an image heightmap using point interpolation with splines
+Assignment 2 represents an image heightmap using point interpolation with Catmull Rom splines. In this program, the 
+depth.bmp image is read using CImg. From there, each pixel of the image is accessed to fetch its colour value. A height
+map is then created to represent the image.
+
+Please note that there are 2 things that aren't functioning properly. First is drawing the points with triangles. For some
+reason, my points change their order (they aren't just respective column and row). I believe it has to do either with how
+the points were stored in my vector or with the way I'm keeping track of the width and height (this can cause the algorithm
+to set a point that's not in the correct spot). Second, my catmull rom isn't giving me exactly what I'm looking for. My
+step 4 (make catmull rom along the x) seems to be working well but it's giving me 3 lines that are out of place. My step
+5 (catmull rom along the z) just isn't working well. There's something about my points or the way I'm accessing them that
+is causing a weird behaviour.
+
+Aside from that, the program is running as it should. I get the original image, the skip size image and, the two catmull 
+rom (even though they aren't 100% perfect). Additionally, I completed the bonus that tracks the original map and can output
+the original with the catmull rom interpolation. I have keyboard control on each step and can constantly change the step 
+and skip size.
+
+The following is the controls for the keyboard:
+--For moving the camera--
+A : movement in the -x
+D : movement in the +x
+W : movement in the -z
+S : movement in the +z
+U : movement in the +y
+J : movement in the -y
+--For changing the steps--
+Z : step 2 (original)
+X : step 3 (skip size)
+C : step 4 (catmull rom along x)
+V : step 5 (catmull rom along z) 
+--For changing the environment--
+T		: to draw in triangles (only works in 'Z' mode since I couldn't generate it well)
+I		: to retake the user inputs
+BACKSPACE	: reset the camera position, goes back to 'Z' and ask for user inputs
+ESC		: close the window
