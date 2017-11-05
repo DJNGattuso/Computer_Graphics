fef0d4fcbeb08f4dd0f98eb668f4989c056450f3
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-05 18:47:08 -0500

    main basic setup

---
+++ Assignment3/rayTracing/rayTracing/main.cpp
@@ -0,0 +1,18 @@
+//Nicholas Gattuso 40007087
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "CImg.h";
+using namespace std;
+
+int main()
+{
+	cout << "main program for ray tracing" << endl;
+	//for testing purpose
+	int n;
+	cin >> n;
+
+	return 0;
+}
