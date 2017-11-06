cb777dee1123bc21a59b4568adf4fd5666c647db
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-05 20:55:07 -0500

    camera reading

--- Assignment3/rayTracing/rayTracing/sceneLoader.h
+++ Assignment3/rayTracing/rayTracing/sceneLoader.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include </Users/Nicholas/Programming/Computer_Graphics/Assignment3/rayTracing/glm/glm.hpp>
 
 class Scene
 {
@@ -16,17 +17,84 @@ public:
 
 		if (sceneStream.is_open()) {
 			std::string Line = "";
-			int lineCount = 0; int maxCount = 0;
+			int lineCount = 0; int maxCount = 0; int objectType = 0;
 			//--------------------loop through the entire txt--------------------
 			while (getline(sceneStream, Line))
 			{
 				sceneCode += "\n" + Line;
+				int lineLength = Line.length() - 1;
 				
 				//---------first line of the txt file------------
-				//if (lineCount == 0)
-				//{
-
-				//}
+				if (lineCount == 0)
+				{
+					objectNumb = Line[0] - '0';
+					objectsCheck = 0;
+					lineCount++;
+				}
+				else //all other lines
+				{
+					if (maxCount == 0) //next object
+					{ 
+						if (Line == "camera") { objectType = 1; maxCount = 4; }
+						else if (Line == "triangle") { objectType = 2; maxCount = 7; }
+						else if (Line == "sphere") { objectType = 3; maxCount = 6; }
+						else if (Line == "light") { objectType = 4; maxCount = 2; }
+						else if (Line == "plane") { objectType = 5; maxCount = 5; }
+						else { objectType = 0; }
+						objectsCheck++;
+					}
+					else //within an objects definition
+					{
+						//Camera
+						if (objectType == 1)
+						{
+							if (maxCount == 4)
+							{
+								camPos.x = Line[5] - '0'; camPos.y = Line[6] - '0'; camPos.z = Line[7] - '0';
+								maxCount--;
+							}
+							else if (maxCount == 3)
+							{
+								camFov = Line[5,6] - '0';
+								maxCount--;
+							}
+							else if (maxCount == 2)
+							{
+								camF = Line[3:lineLength] - '0';
+								maxCount--;
+							}
+							else if (maxCount == 1)
+							{
+								camA = Line[3:lineLength] - '0';
+								maxCount--;
+							}
+						}
+						//triangle
+						else if (objectType == 2)
+						{
+							if (maxCount == 4)
+							{
+								camPos.x = Line[5] - '0'; camPos.y = Line[6] - '0'; camPos.z = Line[7] - '0';
+								maxCount--;
+							}
+							else if (maxCount == 3)
+							{
+								camFov = Line[5, 6] - '0';
+								maxCount--;
+							}
+							else if (maxCount == 2)
+							{
+								camF = Line[3:lineLength] - '0';
+								maxCount--;
+							}
+							else if (maxCount == 1)
+							{
+								camA = Line[3:lineLength] - '0';
+								maxCount--;
+							}
+						}
+					}
+				}
 			}
 			sceneStream.close();
 			loaded = true;
@@ -41,9 +109,19 @@ public:
 	bool getLoad(){ return loaded;}
 
 private:
+	//Check if the file is loaded
 	bool loaded;
-	char objectNumb;
-	
+
+	//Check total number of objects
+	int objectNumb;
+	int objectsCheck;
+
+	//Camera variable
+	glm::vec3 camPos;
+	int camFov;
+	int camF;
+	double camA;
+
 
 };
 
