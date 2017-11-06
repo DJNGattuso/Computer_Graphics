e4243ec5df40b36219205e5b43c5b010a71abeb3
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-05 23:08:45 -0500

    create an image and save the bmp file

--- Assignment3/rayTracing/rayTracing/main.cpp
+++ Assignment3/rayTracing/rayTracing/main.cpp
@@ -20,16 +20,26 @@ int main()
 	if (loadtext.getLoad()) { cout << "read succesfully" << endl; }
 	else { cout << "Read failed" << endl; }
 
-	
+	//must add fetching of variables from scene instead of hardcoding it
 	//-----------------------------------------Create camera object and image space-------------------------------------------------
 	Camera camera(glm::vec3{ 0,0,0 }, 60, 1000, 1.33);
 
 
 
 
-	int n;
-	cin >> n;
+	//-------------------------------------------------Save image----------------------------------------------------------
+	//Creates an image with three channels and sets it to black
+	cimg_library::CImg<float> image(camera.getWidth(), camera.getHeight(), 1, 3, 0);
+	
+	//need to compute the colours
 
+	//Save out the image in BMP format. Pixel values must be in the range [0,255]
+	image.save("render.bmp");
+
+	//Display the rendered image on screen
+	cimg_library::CImgDisplay main_disp(image, "Render");
+	while (!main_disp.is_closed())
+		main_disp.wait();
 
 	return 0;
 }
